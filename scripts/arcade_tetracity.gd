e08da668a3eaf6a212dd890fa72634a1728768ff
Ruxxtin <jamesbglaser13@gmail.com>
2025-01-01 21:01:46 -0600

    arcade stuff

---
+++ scripts/arcade_tetracity.gd
@@ -0,0 +1,7 @@
+extends AudioStreamPlayer2D
+
+@onready var audio_player = $AudioStreamPlayer2D
+
+func _process(delta):
+	# Check if playback_position is greater than 0 to see if audio is playing
+	pass
---
+++ scripts/elec_spike.gd
@@ -0,0 +1,5 @@
+extends AnimatedSprite2D
+
+
+func _on_area_2d_body_entered(body):
+	get_tree().reload_current_scene()
