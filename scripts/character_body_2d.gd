c945319eff82bc29eddb7984fd8cc408b0a43ef8
Ruxxtin <jamesbglaser13@gmail.com>
2025-01-01 18:15:03 -0600

    arcade stuff

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -11,7 +11,7 @@ var sprinting = false
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Sprinting
--- scripts/player_platformer.gd
+++ scripts/player_platformer.gd
@@ -1,18 +1,24 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -410.0
 const GRAVITY = 1330
+var coyote_time = false
 
+@onready var coyote_timer = $CoyoteTimer
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-
+	
+	# Coyote time
+	if is_on_floor():
+		coyote_timer.start()
+		coyote_time = true
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote_time == true):
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_released("jump"):
 		if velocity.y < 0:
@@ -27,3 +33,12 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("glitch_portal"):
+		get_tree().change_scene_to_file("res://scenes/arcade_game_1_internal.tscn")
+
+
+func _on_coyote_timer_timeout():
+	coyote_time = false
