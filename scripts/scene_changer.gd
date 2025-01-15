04b634ef9e3ee3eb9173c7bc81894df4e00a47df
Ruxxtin <jamesbglaser13@gmail.com>
2025-01-14 21:15:44 -0600

    statred almost crafting

--- scripts/scene_changer.gd
+++ scripts/scene_changer.gd
@@ -2,5 +2,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "Player":  # Check if the player triggered the event
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
 		Global.last_building = "basement"  # Name of this building
 		get_tree().change_scene_to_file("res://scenes/sykul_g_floor.tscn")
