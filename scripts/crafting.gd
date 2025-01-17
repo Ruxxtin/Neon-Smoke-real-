8656c044de102f2651d9f8f2b7159c59cff9c182
Ruxxtin <jamesbglaser13@gmail.com>
2025-01-16 21:31:07 -0600

    more crafting a lot

---
+++ scripts/crafting.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+var recipeCount = 0
+var recipeValues = {}
+var recipes = {}
+
+var items = { 3: "Iron",
+			  2: "Battery",
+			  1: "Electromagnet"}
+		
+
+func _ready():
+	load_game()
+
+func createRecipe(rec, nam):
+	recipeValues[recipeCount] = rec
+	recipeCount += 1
+	recipes[str(rec)] = nam
+	print("NEW RECIPE")
+	print(recipes[str(rec)], " : ", rec)
+
+func save():
+	var sav_arr = [recipes, recipeValues, recipeCount]
+	
+	return sav_arr
+
+func save_game():
+	var save_game = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	var json_string = JSON.stringify(save())
+	save_game.store_line(json_string)
+
+func load_game():
+	if not FileAccess.file_exists("user://savegame.save"):
+		return
+	var save_game = FileAccess.open("user://savegame.save", FileAccess.READ)
+	
+	while save_game().get_position() < save_game.get_length():
+		var json_string  = save_game().get_line
+		var json = JSON.new()
+		var parse_result = json.parse(json_string)
+		var node_data = json.get_data()
+		
+		recipes = node_data[0]
+		recipeValues = node_data[1]
+		recipeCount = node_data[2]
+		
--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,5 +1,7 @@
 extends GridContainer
 
+signal item_changed
+
 #region Testing
 @export var ITEM : Item
 @export var ITEM2 : Item
@@ -14,10 +16,18 @@ func add_item(item):
 	for i in get_children():
 		if i.item == null:
 			i.item = item
+			item_changed.emit()
 			return
 
 func remove_item(item):
 	for i in get_children():
 		if i.item == item:
 			i.item = null
+			item_changed.emit()
 			return
+
+func is_available(item):
+	for i in get_children():
+		if i.item == Item:
+			return true
+	return false
