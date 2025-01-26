8e343c03b0c84385f8d07035513ff8b203b02059
Ruxxtin <jamesbglaser13@gmail.com>
2025-01-25 19:31:26 -0600

    more dialogue

--- scripts/cryolab.gd
+++ scripts/cryolab.gd
@@ -5,9 +5,13 @@ extends Node2D
 @onready var player_stuck = $Player_stuck
 @onready var player = $Player
 @onready var camera_2d = $Player/Camera2D
+@onready var ice_punch_1 = $"ice punch 1"
+@onready var ice_punch_2 = $"ice punch 2"
+@onready var ice_punch_3 = $"ice punch 3"
 var punches = 0
 var can_punch = false
 var dialogue_over = false
+var button_can_change = false
 
 func _ready():
 	camera_2d.enabled = false
@@ -22,21 +26,29 @@ func _ready():
 		#	player.position = Vector2(-24, -2)  # Default position
 
 func _physics_process(delta):
+	
 	if Input.is_action_just_pressed("space"):
 		if dialogue_over == true and can_punch == false:
 			can_punch = true
-		elif can_punch == true:
+		if can_punch == true:
 			punches += 1
-	if Input.is_action_pressed("space"):
-		if punches < 2:
+			button_can_change = true
+			if punches == 1:
+				ice_punch_1.play(0.1)
+			if punches == 2:
+				ice_punch_2.play(0)
+			if punches == 3:
+				ice_punch_3.play(0)
+	if Input.is_action_pressed("space") and can_punch == true:
+		if punches < 3 and button_can_change == true:
 			space_button.texture = load("res://sprites/press_space_down.png")
 	else:
-		if punches < 2:
+		if punches < 3:
 			space_button.texture = load("res://sprites/press_space.png")
 	
-	if punches == 1:
+	if punches == 2:
 		cryostasis_transparent.texture = load("res://sprites/cryostasisbroke transparent.png")
-	if punches == 2 and can_punch == true:
+	if punches == 3 and can_punch == true:
 		cryostasis_transparent.texture = load("res://sprites/cryoshattered transparent.png")
 		player.visible = true
 		player.position = Vector2(-24, -2)
@@ -52,3 +64,4 @@ func DialogueSignal(arg: String):
 	if arg == "can_punch":
 		space_button.visible = true
 		dialogue_over = true
+		can_punch = true
