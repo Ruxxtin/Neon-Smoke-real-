8e561ad7cfb783114d27d155e81819ecd0f5a774
Ruxxtin <jamesbglaser13@gmail.com>
2025-01-27 17:10:57 -0600

    wow trash and cutscene wait did i already say trash idk

--- UI/ui.gd
+++ UI/ui.gd
@@ -30,7 +30,7 @@ func _physics_process(_delta):
 			_on_scene_changed(current_scene_path)
 
 func _on_scene_changed(scene_path):
-	if scene_path == "res://scenes/sykul_g_floor_cutscene.tscn":
+	if scene_path == "res://scenes/sykul_g_floor_cutscene.tscn" or scene_path == "res://scenes/panning_cutscene.tscn":
 		visible = false
 	else:
 		visible = true
---
+++ scripts/pan_cam.gd
@@ -0,0 +1,6 @@
+extends Camera2D
+
+var start_pos = Vector2(446,600)
+var end_pos = Vector2(446,-303)
+var start_zoom = 3
+var end_zoom = 1.3
