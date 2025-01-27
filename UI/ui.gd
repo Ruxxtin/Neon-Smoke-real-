182e85c858d2b7bad4579012ab695a68d3cf2334
Ruxxtin <jamesbglaser13@gmail.com>
2025-01-26 23:55:37 -0600

    uh

--- UI/ui.gd
+++ UI/ui.gd
@@ -12,7 +12,6 @@ extends CanvasLayer
 var current_scene = null
 
 func _physics_process(_delta):
-	#if start_checking == true:
 	current_iron.text = str(ResourceManager.iron_amount)
 	current_battery.text = str(ResourceManager.battery_amount)
 	current_wire.text = str(ResourceManager.wire_amount)
@@ -20,6 +19,17 @@ func _physics_process(_delta):
 	current_electromagnet.text = str(ResourceManager.electormagnet_amount)
 	current_circuitboard.text = str(ResourceManager.circuitboard_amount)
 	current_hoverboard.text = str(ResourceManager.hoverboard_amount)
+	
+	var current_scene = get_tree().current_scene
+	if current_scene:
+		# Use metadata or name for identification
+		var current_scene_path = current_scene.get_scene_file_path() if current_scene.has_method("get_scene_file_path") else ""
+		if current_scene_path != previous_scene_path:
+			previous_scene_path = current_scene_path
+			_on_scene_changed(current_scene_path)
+
+func _on_scene_changed(scene_path):
+	if scene_path == 
 
 
 func _on_tree_entered(tree):
